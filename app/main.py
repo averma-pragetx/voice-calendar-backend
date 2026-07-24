e1cb740093b8d55ccc49cd24cfa1858b26d7bba0
ayush-hverma <ayu5hh.verma03@gmail.com>
2026-07-24 17:38:14 +0530

    added frontend origin in cors

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://voice-calendar.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
